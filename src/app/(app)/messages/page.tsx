f011785f5c618356e2124b452c6a69f82d938485
gracymehndiratta <mehndirattagracy62@gmail.com>
2025-07-21 23:52:23 +0530

    fix conflicts

--- src/app/(app)/messages/page.tsx
+++ src/app/(app)/messages/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-
-import ChatList from "@/components/ChatList";
-import ChatWindow from "@/components/ChatWindow";
+import MessagesPageContent from "@/components/ChatPage";
 
 export default function MessagesPage() {
-  return (
-    <div className="flex h-screen w-full overflow-hidden">
-      {/* Left Panel - Chat List */}
-      <ChatList />
-
-      {/* Right Panel - Chat Window */}
-      <div className="flex flex-col flex-1 overflow-hidden bg-[#1e1e2f]">
-        <ChatWindow />
-      </div>
-    </div>
-  );
+  return <MessagesPageContent />;
 }
